5bc80809b95cf5939ba2c34691b0ab4538350779
Shivam Yadav <shivamyadav333221@gmail.com>
2024-03-19 22:37:04 +0530

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0MDUxOA/spectral.js?branch_slug=main&token=e5461730-6f61-41eb-a028-2fb94bef8db2";
+export default { extends: ruleset };
